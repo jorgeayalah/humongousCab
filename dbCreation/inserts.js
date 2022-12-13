9537a3558b7872f2d4aa58ec956bfcd67aa60975
Jorge Ayala <A00828633@itesm.mx>
2022-12-13 22:56:09 +0000

    sevenDrivers and $match

--- dbCreation/inserts.js
+++ dbCreation/inserts.js
@@ -1,56 +1,19 @@
-// //INSERTING EMPLOYEES
-
-// db.employees.insert(
-//     {
-//       "_id" : NumberInt(10001),
-//       "firstName" : "John",
-//       "lastName" : "Higgins",
-//       "info" : {
-//         "phone" : "07426358798",
-//         "workPhone" : "07587933345",
-//         "email" : "johnhiggins789@gmail.com"
-//       },
-//       "address" : {
-//         "postcode" : "E6 9XZ",
-//         "street" : "Bankroft Road",
-//         "houseNumber" : "24a",
-//         "city" : "London"
-//       },
-//       "dob" : new Date("1988-05-20"),
-//       "inCompanySince" : new Date("2017-07-14"),
-//       "licence" : {
-//         "licenceId" : "HIGGI743548DR4ER",
-//         "countryIssued" : "United Kingdom",
-//         "issueDate" : new Date("2005-04-05"),
-//         "expiryDate" : new Date("2022-04-05")
-//       },
-//       "empStatus" : true,
-//       "shiftId" : [NumberInt(1), NumberInt(2), NumberInt(3)],
-//       "salary" : 33500
-  
-//     }
-<<<<<<< Updated upstream
-//   )
-=======
-//   )
-
-
 // INSERTING...
-//  DRIVERS
 
-db.drivers.insert(
+//  DRIVERS
+db.drivers.insertMany([
     {
         "_id" : NumberInt(22060900),
         "name" : "Jalen",
         "lastName" : "Paul",
         "pronouns"  : "he/his/him",
-        "dateOfBirth"   : new Date("1990-12-25"), 
+        "dateOfBirth": new Date("1990-12-25"), 
         "contactDetails": {
             "phone" : "077799822221",
             "workPhone" : "077799822233",
             "email" : "jalenpaul789@gmail.com"
         },
-        "address"   : {
+        "address": {
             "postcode" : "E1 4PU",
             "street" : "Westfield Way",
             "houseNumber" : "20a",
@@ -62,59 +25,43 @@ db.drivers.insert(
             "countryWhereIssued" : "Wales" 
         },
         "previousTwoRoles" : {
-            "roleOne"   : "Secretary",
-            "roleTwo"   : "Driver"
+            "roleOne": "Secretary",
+            "roleTwo": "Driver"
         },
         "disciplinaryProc"  : {
             "amountOfWarnings"  : NumberInt(1),
             "grumpyWhileDriving" : true
         },
-        "shift"  : [false, true, false] //morning, noon or night
-    },
-    {
-        "_id" : NumberInt(22060901),
-        "name" : "Olliver",
-        "lastName" : "Williams",
-        "pronouns"  : "he/his/him",
-        "dateOfBirth"   : new Date("1995-07-20"), 
-        "contactDetails": {
-            "phone" : "077799822228",
-            "workPhone" : "077799822233",
-            "email" : "olliwilliams20@gmail.com"
+        "shift": {
+            "morning": false,
+            "noon"   : true,
+            "night"  : false
         },
-        "address"   : {
-            "postcode" : "E1 4PU",
-            "street" : "Westfield Way",
-            "houseNumber" : "20b",
-            "city" : "London"
-        },
-        "employmentRec" : {
-            "employedSince": new Date("2021-06-20"),
-            "licenceId" : "NDFU957054SM6OJ",
-            "countryWhereIssued" : "England" 
-        },
-        "previousTwoRoles" : {
-            "roleOne"   : "Teacher",
-            "roleTwo"   : "Volunteer"
-        },
-        "disciplinaryProc"  : {
-            "amountOfWarnings"  : NumberInt(0),
-            "grumpyWhileDriving" : true
-        },
-        "shift"  : [false, false, true] //morning, noon or night
+        "revenue": {
+            "grossIncome": NumberInt(3600), // Total revenue drives produces for the company
+            "salary": {
+                "type" : 
+                {
+                    "isFixed": true
+                },
+                "value" : 1200,  // If fixed just substract qty from grossIncome
+                                // else (percentage) then multiply factor value by grossIncome
+                "coin"  : "GBP"
+            }
+        }
     },
     {
         "_id" : NumberInt(22060902),
         "name" : "Jorge",
         "lastName" : "Hernandez",
         "pronouns"  : "he/his/him",
-        "dateOfBirth"   : new Date("1996-11-20"), 
+        "dateOfBirth": new Date("1996-11-20"), 
         "contactDetails": {
             "phone" : "077799822238",
             "workPhone" : "077799822763",
             "email" : "jorge.hdez@gmail.com"
         },
-        "address"   : {
+        "address": {
             "postcode" : "E1 4PU",
             "street" : "Westfield Way",
             "houseNumber" : "20c",
@@ -126,27 +73,139 @@ db.drivers.insert(
             "countryWhereIssued" : "England" 
         },
         "previousTwoRoles" : {
-            "roleOne"   : "Driver",
-            "roleTwo"   : "Volunteer"
+            "roleOne": "Driver",
+            "roleTwo": "Volunteer"
         },
         "disciplinaryProc"  : {
             "amountOfWarnings"  : NumberInt(2),
             "grumpyWhileDriving" : false
         },
-        "shift"  : [false, false, true] //morning, noon or night
+        "shift"  :{
+            "morning": false,
+            "noon"   : false,
+            "night"  : true
+        },
+        "revenue": {
+            "grossIncome": NumberInt(2700), // Total revenue drives produces for the company
+            "salary": {
+                "type" : 
+                {
+                    "isFixed": true
+                },
+                "value" : 1000,  // If fixed just substract qty from grossIncome
+                                // else (percentage) then multiply factor value by grossIncome
+                "coin"  : "GBP"
+            }
+        }
+    },
+    {
+        "_id" : NumberInt(22060901),
+        "name" : "Olliver",
+        "lastName" : "Williams",
+        "pronouns"  : "he/his/him",
+        "dateOfBirth": new Date("1995-07-20"), 
+        "contactDetails": {
+            "phone" : "077799822228",
+            "workPhone" : "077799822233",
+            "email" : "olliwilliams20@gmail.com"
+        },
+        "address": {
+            "postcode" : "E1 4PU",
+            "street" : "Westfield Way",
+            "houseNumber" : "20b",
+            "city" : "London"
+        },
+        "employmentRec" : {
+            "employedSince": new Date("2021-06-20"),
+            "licenceId" : "NDFU957054SM6OJ",
+            "countryWhereIssued" : "England" 
+        },
+        "previousTwoRoles" : {
+            "roleOne": "Teacher",
+            "roleTwo": "Volunteer"
+        },
+        "disciplinaryProc"  : {
+            "amountOfWarnings"  : NumberInt(0),
+            "grumpyWhileDriving" : true
+        },
+        "shift"  :{
+            "morning": false,
+            "noon"   : false,
+            "night"  : true
+        },
+        "revenue": {
+            "grossIncome": NumberInt(3350), // Total revenue drives produces for the company
+            "salary": {
+                "type" : 
+                {
+                    "isFixed": true
+                },
+                "value" : 1100,  // If fixed just substract qty from grossIncome
+                                // else (percentage) then multiply factor value by grossIncome
+                "coin"  : "GBP"
+            }
+        }
     },
     {
         "_id" : NumberInt(22060903),
+        "name" : "Henry",
+        "lastName" : "Scott",
+        "pronouns"  : "he/his/him",
+        "dateOfBirth": new Date("1996-11-20"), 
+        "contactDetails": {
+            "phone" : "077799822238",
+            "workPhone" : "077799822763",
+            "email" : "henry.scott@gmail.com"
+        },
+        "address": {
+            "postcode" : "E1 4PU",
+            "street" : "Westfield Way",
+            "houseNumber" : "20c",
+            "city" : "London"
+        },
+        "employmentRec" : {
+            "employedSince": new Date("2021-03-16"),
+            "licenceId" : "NDFU957054SM8JH",
+            "countryWhereIssued" : "England" 
+        },
+        "previousTwoRoles" : {
+            "roleOne": "Driver",
+            "roleTwo": "Volunteer"
+        },
+        "disciplinaryProc"  : {
+            "amountOfWarnings"  : NumberInt(2),
+            "grumpyWhileDriving" : false
+        },
+        "shift"  :{
+            "morning": false,
+            "noon"   : false,
+            "night"  : true
+        },
+        "revenue": {
+            "grossIncome": NumberInt(3000), // Total revenue drives produces for the company
+            "salary": {
+                "type" : 
+                {
+                    "isFixed": true
+                },
+                "value" : 1000,  // If fixed just substract qty from grossIncome
+                                // else (percentage) then multiply factor value by grossIncome
+                "coin"  : "GBP"
+            }
+        }
+    },
+    {
+        "_id" : NumberInt(22060904),
         "name" : "Christina",
         "lastName" : "Pharrell",
         "pronouns"  : "they/them",
-        "dateOfBirth"   : new Date("1999-05-29"), 
+        "dateOfBirth": new Date("1999-05-29"), 
         "contactDetails": {
             "phone" : "077799822321",
             "workPhone" : "077799822321",
             "email" : "pharrell.christi@gmail.com"
         },
-        "address"   : {
+        "address": {
             "postcode" : "E1 4PU",
             "street" : "Westfield Way",
             "houseNumber" : "20d",
@@ -158,59 +217,43 @@ db.drivers.insert(
             "countryWhereIssued" : "England" 
         },
         "previousTwoRoles" : {
-            "roleOne"   : "Teacher",
-            "roleTwo"   : "Volunteer"
+            "roleOne": "Teacher",
+            "roleTwo": "Volunteer"
         },
         "disciplinaryProc"  : {
             "amountOfWarnings"  : NumberInt(0),
             "grumpyWhileDriving" : true
         },
-        "shift"  : [false, false, true] //morning, noon or night
-    },
-    {
-        "_id" : NumberInt(22060904),
-        "name" : "Alice",
-        "lastName" : "Crisp",
-        "pronouns"  : "she/her",
-        "dateOfBirth"   : new Date("1994-10-21"), 
-        "contactDetails": {
-            "phone" : "077799820123",
-            "workPhone" : "077799820123",
-            "email" : "aalice.crispp@gmail.com"
+        "shift"  :{
+            "morning": false,
+            "noon"   : false,
+            "night"  : true
         },
-        "address"   : {
-            "postcode" : "E1 4PU",
-            "street" : "Westfield Way",
-            "houseNumber" : "20e",
-            "city" : "London"
-        },
-        "employmentRec" : {
-            "employedSince": new Date("2021-01-08"),
-            "licenceId" : "NDFU957054SM7AC",
-            "countryWhereIssued" : "England" 
-        },
-        "previousTwoRoles" : {
-            "roleOne"   : "Teacher",
-            "roleTwo"   : "Chef"
-        },
-        "disciplinaryProc"  : {
-            "amountOfWarnings"  : NumberInt(0),
-            "grumpyWhileDriving" : false
-        },
-        "shift"  : [true, false, false] //morning, noon or night
+        "revenue": {
+            "grossIncome": NumberInt(3400), // Total revenue drives produces for the company
+            "salary": {
+                "type" : 
+                {
+                    "isFixed": false
+                },
+                "value" : 0.35, // If fixed just substract qty from grossIncome
+                                // else (percentage) then multiply factor value by grossIncome
+                "coin"  : "GBP"
+            }
+        }
     },
     {
         "_id" : NumberInt(22060905),
         "name" : "Alice",
         "lastName" : "Crisp",
         "pronouns"  : "she/her",
-        "dateOfBirth"   : new Date("1994-10-21"), 
+        "dateOfBirth": new Date("1994-10-21"), 
         "contactDetails": {
             "phone" : "077799820123",
             "workPhone" : "077799820123",
             "email" : "aalice.crispp@gmail.com"
         },
-        "address"   : {
+        "address": {
             "postcode" : "E1 4PU",
             "street" : "Westfield Way",
             "houseNumber" : "20e",
@@ -222,16 +265,101 @@ db.drivers.insert(
             "countryWhereIssued" : "England" 
         },
         "previousTwoRoles" : {
-            "roleOne"   : "Teacher",
-            "roleTwo"   : "Chef"
+            "roleOne": "Teacher",
+            "roleTwo": "Chef"
         },
         "disciplinaryProc"  : {
             "amountOfWarnings"  : NumberInt(0),
             "grumpyWhileDriving" : false
         },
-        "shift"  : [true, false, false] //morning, noon or night
+        "shift"  :{
+            "morning": true,
+            "noon"   : false,
+            "night"  : false
+        },
+        "revenue": {
+            "grossIncome": NumberInt(3000), // Total revenue drives produces for the company
+            "salary": {
+                "type" : 
+                {
+                    "isFixed": true
+                },
+                "value" : 0.33, // If fixed just substract qty from grossIncome
+                                // else (percentage) then multiply factor value by grossIncome
+                "coin"  : "GBP"
+            }
+        }
+    },
+    {
+        "_id" : NumberInt(22060906),
+        "name" : "Muhammad",
+        "lastName" : "Gill",
+        "pronouns"  : "he/him/his",
+        "dateOfBirth": new Date("1994-10-21"), 
+        "contactDetails": {
+            "phone" : "077799820129",
+            "workPhone" : "077799820129",
+            "email" : "muhammad.g@gmail.com"
+        },
+        "address": {
+            "postcode" : "E1 4PU",
+            "street" : "Westfield Way",
+            "houseNumber" : "20e",
+            "city" : "London"
+        },
+        "employmentRec" : {
+            "employedSince": new Date("2021-01-08"),
+            "licenceId" : "NDFU957054SM7AC",
+            "countryWhereIssued" : "England" 
+        },
+        "previousTwoRoles" : {
+            "roleOne": "Teacher",
+            "roleTwo": "Demonstrator"
+        },
+        "disciplinaryProc"  : {
+            "amountOfWarnings"  : NumberInt(0),
+            "grumpyWhileDriving" : false
+        },
+        "shift"  :{
+            "morning": true,
+            "noon"   : false,
+            "night"  : false
+        },
+        "revenue": {
+            "grossIncome": NumberInt(3300), // Total revenue drives produces for the company
+            "salary": {
+                "type" : 
+                {
+                    "isFixed": true
+                },
+                "value" : 0.34, // If fixed just substract qty from grossIncome
+                                // else (percentage) then multiply factor value by grossIncome
+                "coin"  : "GBP"
+            }
+        }
     }
-)
+])
 
 //  CARS
->>>>>>> Stashed changes
+
+
+// QUERIES
+
+// db.drivers.aggregate([{     //this $match notation works for more than two or more embedded document fields
+//     '$match': {
+//         "revenue": {
+//             "salary": {
+//                 "type": {
+//                     "isFixed": false,
+//                     "anotherField": true
+//                 }
+//             }
+//         }
+//     }
+// }])
+
+db.drivers.aggregate([{     //the dot notation works for $match when only looking for an exact embedded document field 
+    '$match': {
+        "revenue.salary.type.isFixed": false
+    }
+}])
